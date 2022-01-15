668d057b6cff7a71ca3570220dcb1dcbb3b11ae0
Lexi <lexi@qixils.dev>
2022-01-14 23:00:07 -0500

    simple ping% anticheat

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # gdq-discord-schedule
 
-This is a Discord bot that creates a mirror of the GDQ schedule in a specified text channel, complete with VOD links,
-runners, start times, donation incentive progress, bid war options, and more.
+This is a Discord bot that creates a mirror of the GDQ schedule in a specified text channel,
+complete with VOD links, runners, start times, donation incentive progress, bid war options, and
+more.
 
 ![Schedule Example](https://i.imgur.com/KdHDlGq.png)
 
-It additionally posts an embed at the end of the channel listing the current run, the upcoming 3 runs (configurable), and when they will start.
+It additionally posts an embed at the end of the channel listing the current run, the upcoming 3
+runs (configurable), and when they will start.
 
 ![Embed Example](https://i.imgur.com/FrF554b.png)
 
 The bot respects GDQ's rate limits as best as it can by waiting 2.5 seconds between each API call.
-Requests are minimal as it only calls the list of runs, donation incentives/bid wars, bid war options, and runner information once per loop.
+Requests are minimal as it only calls the list of runs, donation incentives/bid wars, bid war
+options, and runner information once per loop.
 
 ### main.py
 
@@ -18,8 +21,8 @@ This is the script that runs the schedule creator and updater with the above fea
 
 ### games.py
 
-This is a very hardcoded side-project that runs various games related to the donation total of the event.
-Most should probably ignore this.
+This is a very hardcoded side-project that runs various games related to the donation total of the
+event. Most should probably ignore this.
 
 ## Usage
 
@@ -27,7 +30,7 @@ Most should probably ignore this.
 
 Use `pip install -U -r requirements.txt` to install the required dependencies.
 
-Copy `example_config.yaml` to `config.yaml` and change the values as appropriate.
-Detailed descriptions of each value are included in comments.
+Copy `example_config.yaml` to `config.yaml` and change the values as appropriate. Detailed
+descriptions of each value are included in comments.
 
 Run with `python3 main.py`
---
+++ anticheat.py
@@ -0,0 +1,94 @@
+import asyncio
+import re
+
+import aiohttp
+import discord
+from yaml import load
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+
+class DiscordClient(discord.Client):
+    # request headers
+    gdq_headers = {"headers": {"User-Agent": "rush-schedule-updater"}}
+    murphy_ping = re.compile(r"<@!?187684157181132800>")
+    channel_id = 442082610785550337
+    amount_regex = re.compile(r"^\$?((?:\d{1,3},?){1,3}(?:\.\d+)?)(?: ?([MKmk]))?")
+    suffix_map = {
+        'm': 1000000,
+        'k': 1000
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.config = load(open('config.yaml', 'r'), Loader)
+        # aiohttp session, do not change
+        self.session: aiohttp.ClientSession = None  # gets defined later because it yelled at me for creating in non-async func
+
+    async def load_gdq_json(self, query):
+        """
+        Loads and processes a GDQ API page
+        :param query: the search parameters to query
+        :return: json object
+        """
+        url = f"{self.config['gdq_url']}{query}"
+        async with self.session.get(url, **self.gdq_headers) as r:
+            if r.status == 200:
+                jsondata = await r.json()
+
+                # GDQ doesn't provide official ratelimits, so we apply our own safe amount
+                await asyncio.sleep(2.5)
+            else:
+                print("GET {} returned {} {} -- aborting".format(url, r.status, await r.text()))
+                exit()
+        return jsondata
+
+    async def load_gdq_index(self):
+        """
+        Returns the GDQ index (main) page, includes donation totals
+        :return: json object
+        """
+        return (await self.load_gdq_json(f"?type=event&id={self.config['event_id']}"))[0]['fields']
+
+    async def load_donation_total(self) -> float:
+        """
+        Returns the current GDQ donation total
+        :return: float
+        """
+        return float((await self.load_gdq_index())['amount'])
+
+    async def on_ready(self):
+        self.session = aiohttp.ClientSession()
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, msg: discord.Message):
+        if self.session is None:
+            return
+        if msg.channel.id != self.channel_id:
+            return
+        content = self.murphy_ping.sub("", msg.content).strip()
+        if content == msg.content or len(content) == 0:
+            return
+        match = self.amount_regex.match(content)
+        if not match:
+            return
+        amount = float(match.group(1).replace(',', ''))
+        if match.group(1):
+            amount *= self.suffix_map[match.group(1).lower()]
+
+        current_amount = await self.load_donation_total()
+        # conversion to int gives users benefit of the doubt in regard to rounding errors
+        if int(amount) >= int(current_amount):
+            return
+
+        await msg.reply(f"liar! >:( we're at only ${current_amount:,.2f}, not ${amount:,.2f}.")
+
+
+client = DiscordClient(allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False))
+client.run(client.config['token'], bot=True)
